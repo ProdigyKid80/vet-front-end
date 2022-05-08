59b6fec8128e0c6766272bc3fc0a7115d61b978c
ProdigyKid80 <song.rut@gmail.com>
2022-05-08 20:55:55 +0300

    added dashboard and logs html and css

---
+++ assets/scripts/logs.js
@@ -0,0 +1,7 @@
+document.getElementById("logFilter").addEventListener("click", () => {
+  document.getElementById("logFilter").classList.toggle("btn-inactive");
+});
+
+document.getElementById("prescFilter").addEventListener("click", () => {
+  document.getElementById("prescFilter").classList.toggle("btn-inactive");
+});
